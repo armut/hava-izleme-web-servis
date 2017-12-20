1645544d6d3590647ae5217dee5c60d15843b3bc
abdullah <abdullah.akalin@gazi.edu.tr>
2017-12-20 19:55:22 +0300

    Tüm istasyonların ölçümlerinin alındığı bir view yazıldı.
    * Ayrıca istasyon listesinin alındığı bir view yazıldı.

--- cbs/urls.py
+++ cbs/urls.py
@@ -5,5 +5,6 @@ from kirli.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path(r'api/', get_station_report, name='station_report')
+    path(r'api/kirli/', get_station_report, name='station_report'),
+    path(r'api/kirli/istasyon_listesi', get_station_list, name='station_list'),
 ]
--- kirli/views.py
+++ kirli/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from tools import *
+import tools
 import json
+from time import sleep
 
 def get_station_report(request):
     if request.method == 'GET':
-        if 'station' in request.GET and 'date' in request.GET:
-            return JsonResponse(parser(request.GET.get('station'), request.GET.get('date')))
+        if 'istasyon' in request.GET and 'tarih' in request.GET:
+            if request.GET.get('istasyon') == 'hepsi':
+                result = {}
+                for station in tools.station_dict.keys():
+                    try:
+                        result[station] = tools.parser(station, request.GET.get('tarih'))
+                    except requests.exceptions.ConnectionError:
+                        return JsonResponse({'Hata': 'Sunucu cevap vermeyi reddetti.'})
+                return JsonResponse(result)
+            else:
+                try:
+                    return JsonResponse(tools.parser(request.GET.get('istasyon'), request.GET.get('tarih')))
+                except:
+                    return JsonResponse({
+                        'Hata': 'Hatayı açıklamayan hata mesajlarından ben de hiç hoşlanmıyorum ama hatanın sebebini gerçekten bilmiyorum.'
+                    })
+        
+def get_station_list(request):
+    if request.method == 'GET':
+        return JsonResponse(tools.station_dict)
+                
